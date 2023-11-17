67b5331715b08148e1590ead1e88f9a0bee4ef0c
Luis Lopez <luis.lopez3664mail.com>
2023-11-17 19:26:36 -0300

    codigo de clases MedioDeNotificacion: Mail y Whatsapp

--- src/main/java/RRHH/Whatsapp.java
+++ src/main/java/RRHH/Whatsapp.java
@@ -1,4 +1,15 @@
 package RRHH;
 
-public class Whatsapp {
+import MesaDeEntrada.Notificacion;
+
+public class Whatsapp extends MedioDeNotificacion {
+
+    public Whatsapp(String nombre, String medio) {
+        super(nombre, medio);
+    }
+
+    @Override
+    public void enviarNotificacion(Notificacion notificacion) {
+        System.out.println("Notificacion enviada por Whatsapp al numero " + super.getMedio() + " con el mensaje: " + notificacion.toString());
+    }
 }
