3bb5634f75695daf46466ec2bc4590e4d30cb7d6
Franco Libertini <libertini.f.a@gmail.com>
2023-11-17 23:05:20 -0300

    Carga de clase Tecnico

--- src/main/java/RRHH/Tecnico.java
+++ src/main/java/RRHH/Tecnico.java
@@ -1,4 +1,59 @@
 package RRHH;
 
+import MesaDeEntrada.Incidente;
+import MesaDeEntrada.Notificacion;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Entity
+@Table(name = "tecnico")
+@Setter
+@Getter
 public class Tecnico {
+
+    @Id
+    @Column(name = "id_tecnico")
+    @GeneratedValue(strategy = SEQUENCE, generator = "ID_SEQ")
+    private Integer id;
+
+    @Column(name = "cuit")
+    private String cuit;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @ManyToOne
+    @JoinColumn(name = "medio_de_notificacion_id_medio_de_notificacion")
+    private MedioDeNotificacion medioDeNotificacionPreferido;
+
+    @OneToMany(mappedBy = "tecnico")
+    private List<Especialidad> especialidades;
+
+    @OneToMany(mappedBy = "tecnico")
+    private List<Incidente> incidentes;
+    private Notificacion notificacion;
+
+    public Tecnico(String cuit, String nombre, MedioDeNotificacion medioDeNotificacionPreferido, List<Especialidad> especialidades, List<Incidente> incidentes) {
+        this.cuit = cuit;
+        this.nombre = nombre;
+        this.medioDeNotificacionPreferido = medioDeNotificacionPreferido;
+        this.especialidades = especialidades;
+        this.incidentes = incidentes;
+    }
+
+    public void setMedioDeNotificacionPreferido(String medioDeNotificacionPreferido) {
+        //TODO
+
+    }
+
+    public void notificarProblema(Notificacion notificacion) {
+        this.notificacion = notificacion;
+        //TODO
+
+    }
 }
