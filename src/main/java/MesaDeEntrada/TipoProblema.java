002939ec3ad5502be9dd9af9ea8f5c3f891ee79f
Franco Libertini <libertini.f.a@gmail.com>
2023-11-27 23:39:07 -0300

    Agregado de arrays en Cliente y TipoProblema

--- src/main/java/MesaDeEntrada/TipoProblema.java
+++ src/main/java/MesaDeEntrada/TipoProblema.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static javax.persistence.GenerationType.SEQUENCE;
@@ -36,7 +37,7 @@ public class TipoProblema {
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.tiempoDeResolucionMaximo = tiempoDeResolucionMaximo;
-        this.especialidades = especialidades;
+        this.especialidades = new ArrayList<>();
     }
 
     public void agregarEspecialidad(Especialidad especialidad){
